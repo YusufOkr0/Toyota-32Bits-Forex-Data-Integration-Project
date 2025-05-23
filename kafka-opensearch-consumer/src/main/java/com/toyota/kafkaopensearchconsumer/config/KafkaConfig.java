d8d7ba22197098b92e05d5af6041dd02ee7bdcba
YusufOkr0 <Yusufokr0@gmail.com>
2025-05-23 16:22:38 +0300

    Start to create a kafka consumer to send exchange rates to opensearch.

---
+++ kafka-opensearch-consumer/src/main/java/com/toyota/kafkaopensearchconsumer/config/KafkaConfig.java
@@ -0,0 +1,11 @@
+package com.toyota.kafkaopensearchconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+
+
+}
